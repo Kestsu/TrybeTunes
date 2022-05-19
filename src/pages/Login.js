a9c11bf9524ef73f3a624ddf50bd06b66893f342
Kevin Li <Kevinstart14@gmail.com>
2022-05-19 16:56:29 -0300

    feito 1

---
+++ src/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">...</div>
+    );
+  }
+}
+
+export default Login;
+
+// <form>
+//   <h2>Insira seu nome</h2>
+//   <input
+//     data-testid="login-name-input"
+//     type="text"
+//     placeholder="Digite seu nome"
+//   />
+//   <button type="button" data-testid="login-submit-button">Entrar</button>
+// </form>
